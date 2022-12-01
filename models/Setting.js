f8b5a2aac0367b0d06c9c07c4454d817b90e4753
Sergey Lukovkin <slukovkin@icloud.com>
2022-12-01 13:41:27 +0200

    update

--- models/Setting.js
+++ models/Setting.js
@@ -1,29 +1,30 @@
-const {Sequelize, Model, DataTypes} = require('sequelize')
+const { Sequelize, Model, DataTypes } = require("sequelize");
 
 class Setting extends Model {}
 
-Setting.init({
-  // Model attributes are defined here
-  eprice: {
-    type: DataTypes.FLOAT,
-    allowNull: true
+Setting.init(
+  {
+    // Model attributes are defined here
+    eprice: {
+      type: DataTypes.FLOAT,
+      allowNull: true,
+    },
+    wprice: {
+      type: DataTypes.FLOAT,
+      allowNull: true,
+    },
+    gprice: {
+      type: DataTypes.FLOAT,
+      allowNull: true,
+    },
   },
-  wprice: {
-    type: DataTypes.FLOAT,
-    allowNull: true
-  },
-  gprice: {
-  	type: DataTypes.FLOAT,
-  	allowNull: true
+  {
+    timestamps: true,
+    sequelize,
+    modelName: "Setting",
   }
-}, {
-  timestamps: true,
-  sequelize, 
-  modelName: 'Setting' 
-});
+);
 
-
-// console.log(Setting === sequelize.models.Setting);
 module.exports = {
-	Setting
-}
+  Setting,
+};
