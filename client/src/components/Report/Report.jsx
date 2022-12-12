d5ef72efb77367ee00a57eb7a3a30b8a6d6e9389
Sergey Lukovkin <slukovkin@icloud.com>
2022-12-13 01:17:42 +0200

    create form report

--- client/src/components/Report/Report.jsx
+++ client/src/components/Report/Report.jsx
@@ -1,13 +1,33 @@
-import React from 'react'
+import React from "react";
+import "./Report.module.css";
 
 function Report(props) {
-
-  React.useEffect(()=> {
-    document.title = props.title
-  })
+  React.useEffect(() => {
+    document.title = props.title;
+  });
   return (
-    <div>Report</div>
-  )
+    <>
+      <table>
+        <tbody>
+          <h2>Журнал оплат</h2>
+          <th>Назначение платежа</th>
+          <th>Предыдущий счетчик</th>
+          <th>Текущий счетчик</th>
+          <th>Кол-во</th>
+          <th>Тариф</th>
+          <th>Сумма</th>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+    </>
+  );
 }
 
-export default Report
+export default Report;
